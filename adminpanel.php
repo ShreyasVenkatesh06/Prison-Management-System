de5bea973b73632677e585be0e40038de67f4660
ShreyasVenkatesh06 <44697058+ShreyasVenkatesh06@users.noreply.github.com>
2018-11-28 21:02:54 +0530

    Add files via upload

---
+++ adminpanel.php
@@ -0,0 +1,99 @@
+
+
+<head>
+  <title>ADMIN</title>
+   <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+<body>
+	<table align="center" border="0" bgcolor="black" width="100%" height='100%' cellpadding="8" cellspacing="0" height="500">
+          
+		  <tr>
+            <td colspan='3' width='100%' height='2'><img src='2.gif'></td>
+          </tr>
+		  <tr>
+            <td colspan="7" bgcolor="black" height="1" align="left">
+		      <h1><font size="5">
+	          
+               <a href="registration.php">RegisterPrisoner</a>| | |
+		       <a href="transfer.php">PrisonerTrans </a> | | |
+			   <a href="court.php">Court</a>| | |
+			   <a href="search-form.php">Search</a> | | |
+			   <a href="sp.php">OfficerDetails</a>| | |
+		        <a href="officertransfer.php">OfficerTrans</a> 
+		        </font></h1>
+            </td>
+			 </td>
+		 
+            <td height='1' colspan='3' align='right' bgcolor="black">&nbsp;</td>
+			
+          </tr>
+		 
+          <tr>
+		 
+            <td width='4%' bgcolor='black' valign='top'>
+<h3 align='center'  title='You should be online'>&nbsp;</h3></td>
+
+            <td width='71%' valign='top' bgcolor="white">
+
+<p align='center'>
+ 
+
+<h3 align='center'>&nbsp;</h3>
+<br/>
+<h3 align='center'>WELCOME ADMINISTRATOR</h3>
+<P align='justify'><font face='Arial, Helvetica, sans-serif'>The system enables an Administrator to provide services to users and here the administrator can add and upload information, delete, view the record added.</font></p>
+
+		<br>
+			</td>
+            <td width='25%' bgcolor='black'  valign='top'>
+			
+	
+<table border='0' align='' >
+<tr>
+<td width="250" bgcolor="Black">
+<h3> Admin Management  </h3>
+
+
+
+
+<ul>
+<tr>
+            <td colspan="7" bgcolor="black" height="1" align="center">
+		      <h1><font size="5">
+			  <a href='viewprisoners.php'><b>Prisoner  Information</a> |<br><br>
+			   <a href='viewtransfer.php'><b>View Prisoner Transfer </a><br><br>
+			   <a href='viewcase.php'><b>Case Information</a> |<br><br>
+	           <a href='viewofficer.php'><b>View Officer Transfer</a>|<br><br>
+               <a href='bkp.php'><b>Prisoner Backup </a> <br><br>
+	<br>
+	
+    <a href='index.php''>LOGOUT</a>
+
+				
+		        </font></h1>
+            </td>
+			 </td>
+		 
+            <td height='1' colspan='3' align='right' bgcolor="black">&nbsp;</td>
+			
+          </tr>
+
+
+</ul>
+</td>
+</tr>
+</table>
+
+
+			
+			</td>
+          </tr>
+          <tr>
+            <td colspan='3' align='center' bgcolor='black' height='1'>
+					<strong>
+                YS2</strong></td>
+          </tr>
+	</table>
+</body>
+</head>
+</html>
