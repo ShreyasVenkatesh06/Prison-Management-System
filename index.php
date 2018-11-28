5c5dce55f428f849334eb4e527d4838fe28c8a72
ShreyasVenkatesh06 <44697058+ShreyasVenkatesh06@users.noreply.github.com>
2018-11-28 21:10:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+   
+  <title>Prison Services</title>
+  <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+<body>
+
+	<table align="center" border="0" bgcolor="black" width="100%" height='100%' cellpadding="8" cellspacing="0" height="100">
+          <tr>
+            <td colspan="3" height="2"><img src="2.gif" width="100%"></td>
+          </tr>
+          <tr>
+            <td colspan="7" bgcolor="black" height="1" align="center">
+		      <h1><font size="5">	 
+		        <a href="login2.php">Member Login</a>|<>
+				<a href="register.php">Register Admin</a>|<>
+		      
+		         
+		        </font></h1>
+            </td>
+</tr>
+
+<td width="91%" height="387" valign="top" bgcolor="#FFFFFF"><h2> <center>DBMS MINI-PROJECT<center/></h2>
+  <p><B>A simple prison management system,which provides login for two different personnel: Administrator & Police.
+  The Administrator has the privileges of adding a prisoner,adding an officer,tranfer a prisoner,tranfer an officer,add court details of a prisoner and deleting the records.
+  The Police has the authority to only view the records related to a prisoner.</B></p>
+  <p><strong></strong></p>
+  
+
+  <div><img src="4.jpg" width="200" height="170"  alt=""/> <img src="3.jpg" width="300" height="170"  alt=""/> <img src="2.jpg" width="300" height="170"  alt=""/> <img src="6.jpg" width="200" height="170"  alt=""/></div></td>
+
+<td width="30%" height="387" valign="top" bgcolor="white">
+</td>
+
+<td bgcolor="white"></td>
+<td width="30%" bgcolor="white">
+
+ <tr>
+            <td colspan='3' align='center' bgcolor='white' height='1'>
+					&copy; <strong>
+                YS2</strong></td>
+          </tr>
+</table>
+</body>
+</html>
---
+++ officerpanel.php
@@ -0,0 +1,72 @@
+
+
+<head>
+  <title> PRISON SERVICES</title>
+   <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+	<table align="center" border="0" bgcolor="black" width="100%" height='100%' cellpadding="9" cellspacing="0" height="525">
+          <tr>
+            <td colspan="3" height="2"><img src="2.gif" width="100%"></td>
+          </tr>
+          <tr>
+            <td colspan="3" bgcolor="white" height="1" align="center">
+		<font size="4">
+        
+      
+          </font>
+            </td>
+          </tr>
+		 
+          <tr>
+            <td width='4%' bgcolor='#FFFFFF' valign='top'>
+
+
+            <td width='71%' valign='top' bgcolor="#FFFFFF">
+
+
+
+<h3 align='center'>&nbsp;</h3>
+<br/>
+<h3 align='center'>WELCOME OFFICER </h3>
+<P align=''FACE'><font=54 face='Arial, Helvetica, sans-serif'>This is the home page of an officer.The officer can only view the information of prisoners.</font></p>
+
+			</td>
+            <td width='25%' bgcolor='#FFFFFF'  valign='top'>
+			
+	
+<table border='0' align='center'>
+<tr>
+<td width="252"  bgcolor='black' ">
+
+
+
+
+	<>
+		
+	<h3><a href='viewprisoners1.php'><b>Prisoners Information</b></a></h3>
+		<>
+		
+	<h3><a href='viewcourt1.php'><b>Court Information</b></a></h3>
+		<>
+	<h3><a href='viewtransfer1.php'><b>Prisoners Transfer </b></a></h3>
+		<>
+    <li><a href='index.php'><b>LOG OUT</b></a></li>
+	
+
+</td>
+</tr>
+</table>
+
+
+			
+			</td>
+          </tr>
+          <tr>
+            <td colspan='3' align='center' bgcolor='white' height='1'>
+					&copy; <strong>
+                YS2</strong></td>
+          </tr>
+	</table>
+</body>
+</head>
+</html>
